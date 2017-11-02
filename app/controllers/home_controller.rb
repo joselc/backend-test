461f75856730f39228356f2f8bea7c1faa3b3d53
Jose Lopez <jose.lopez226@gmail.com>
2017-11-02 22:37:22 +0100

    BCKD-TEST: final solution

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,27 +7,13 @@ class HomeController < ApplicationController
     # LEFT JOIN groups_users gu ON gu.user_id = u.id
     # LEFT JOIN groups g ON g.id = gu.group_id
     # GROUP BY u.id
-    # ORDER BY gc ASC;
-    @users = User.all
-
-    # CHALLENGE 2
-    # SELECT g.name, u.name, SUM(m.mapviews) as mv
-    # FROM maps m
-    # LEFT JOIN users u ON m.user_id = u.id
-    # LEFT JOIN groups_users gu ON u.id = gu.user_id
-    # LEFT JOIN groups g ON gu.group_id = g.id
-    # GROUP BY g.name, u.name
-    # ORDER BY g.name, mv DESC;
-
-    # CHALLENGE 3
-    # SELECT g.name, u.name, SUM(m.mapviews) as mv,
-    # SUM(m.mapviews)/SUM(SUM(m.mapviews)) OVER (PARTITION BY g.id) *100 as mv2
-    # FROM maps m
-    # INNER JOIN users u ON m.user_id = u.id
-    # LEFT JOIN groups_users gu ON u.id = gu.user_id
-    # LEFT JOIN groups g ON gu.group_id = g.id
-    # GROUP BY g.id, u.id
-    # ORDER BY g.name, mv DESC;
-
+    @users = Array.new
+    User.select("users.name, users.created_at, count(distinct(groups.id)) as gc, string_agg(groups.name, ', ') as gl")
+    .joins("LEFT JOIN groups_users ON groups_users.user_id = users.id LEFT JOIN groups ON groups.id = groups_users.group_id")
+    .group("users.id")
+    .find_each(:batch_size => 10) do |user|
+      @users.push(user)
+    end
+    @users = @users.sort_by{|x| -x.gc}
   end
 end
